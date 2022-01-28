6cf90e98d3c61e352ba88aca05d218cd1de16032
Tatiana Sierkova <tanuha_86@ukr.net>
2022-01-28 21:58:59 +0200

    !!!!!!

--- js/task-01.js
+++ js/task-01.js
@@ -1,4 +1,27 @@
 const categoriesEl = document.querySelectorAll('li.item');
-
 const categoriesElList = categoriesEl.length;
-console.log(`Number of categories:`, categoriesElList)
+console.log(`Number of categories:`, categoriesElList);
+
+const firstTitle = document.querySelectorAll('h2');
+firstTitle.textContent = `Animals`;
+console.log(`Category:`, firstTitle.textContent);
+
+const titleLi = document.querySelectorAll('ul');
+const categoriesElItem = titleLi.length;
+console.log(`Elements:`, categoriesElItem);
+
+const secondTitle = document.querySelectorAll('h2');
+secondTitle.textContent = `Products`;
+console.log(`Category:`, secondTitle.textContent);
+
+//const titleLi = document.querySelectorAll('ul');
+//const categoriesElItem = titleLi.length;
+//console.log(`Elements:`, categoriesElItem);
+
+const thirdTitle = document.querySelectorAll('h2');
+thirdTitle.textContent = `Technologies`;
+console.log(`Category:`, thirdTitle.textContent);
+
+//const titleLi = document.querySelectorAll('ul');
+//const categoriesElItem = titleLi.length;
+//console.log(`Elements:`, categoriesElItem);
