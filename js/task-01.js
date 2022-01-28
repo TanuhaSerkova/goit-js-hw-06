57fecc38eddfc54ac6366127da9272bd339c821e
Tatiana Sierkova <tanuha_86@ukr.net>
2022-01-28 23:49:02 +0200

    task1-end

--- js/task-01.js
+++ js/task-01.js
@@ -1,27 +1,13 @@
-const categoriesEl = document.querySelectorAll('li.item');
+const categoriesEl = document.querySelectorAll('ul#categories li.item');
 const categoriesElList = categoriesEl.length;
 console.log(`Number of categories:`, categoriesElList);
 
-const firstTitle = document.querySelectorAll('h2');
-firstTitle.textContent = `Animals`;
-console.log(`Category:`, firstTitle.textContent);
 
-const titleLi = document.querySelectorAll('ul');
-const categoriesElItem = titleLi.length;
-console.log(`Elements:`, categoriesElItem);
+for (let i = 0; i < categoriesElList; i += 1) { 
+    const categoriesText = categoriesEl[i].firstElementChild.textContent;
+    console.log(`Category:`, categoriesText);
 
-const secondTitle = document.querySelectorAll('h2');
-secondTitle.textContent = `Products`;
-console.log(`Category:`, secondTitle.textContent);
-
-//const titleLi = document.querySelectorAll('ul');
-//const categoriesElItem = titleLi.length;
-//console.log(`Elements:`, categoriesElItem);
-
-const thirdTitle = document.querySelectorAll('h2');
-thirdTitle.textContent = `Technologies`;
-console.log(`Category:`, thirdTitle.textContent);
-
-//const titleLi = document.querySelectorAll('ul');
-//const categoriesElItem = titleLi.length;
-//console.log(`Elements:`, categoriesElItem);
+    const listEl = categoriesEl[i].lastElementChild;
+    const numbersElList = listEl.querySelectorAll('li');
+    console.log(`Elements:`, numbersElList.length);
+};
