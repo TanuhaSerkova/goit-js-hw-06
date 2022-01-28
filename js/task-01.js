2df40b289c695e7af2167b42615584e517082b8c
Tatiana Sierkova <tanuha_86@ukr.net>
2022-01-28 19:03:27 +0200

    task1

--- js/task-01.js
+++ js/task-01.js
@@ -1,1 +1,4 @@
-const categoriesEl = document.querySelector('.item');
+const categoriesEl = document.querySelectorAll('li.item');
+
+const categoriesElList = categoriesEl.length;
+console.log(`Number of categories:`, categoriesElList)
