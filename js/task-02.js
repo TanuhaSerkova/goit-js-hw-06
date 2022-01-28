b636fd426d0fd2ad1a161ac532b735b3f29f4c6f
Tatiana Sierkova <tanuha_86@ukr.net>
2022-01-29 00:25:33 +0200

    fe

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,10 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsEl = document.querySelector(`#ingredients`);
+console.log(ingredientsEl);
+const ingredientsList = document.createElement("li");
+igredientsList.textContent = ingredients;
+list.append(igredientsList);
+console.log(igredientsList.textContent);
