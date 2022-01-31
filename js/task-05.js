966f60b7fe68dec4338157ec2867cfc2b065fa05
Tatiana Sierkova <tanuha_86@ukr.net>
2022-01-31 15:32:49 +0200

    task05-end

--- js/task-05.js
+++ js/task-05.js
@@ -1,11 +1,11 @@
 let inputRef = document.querySelector(`#name-input`);
 let outInput = document.querySelector(`#name-output`);
 
-inputRef.addEventListener('input', onInputChange);
+inputRef.addEventListener(`input`, onInputChange);
 
 function onInputChange(event) { 
     inputRef = event.currentTarget.value;
     console.log(inputRef);
 
-    inputRef !== ' ' ? (outInput.innerText = inputRef.trim()) : `Anonymous`;
+    inputRef !== ` ` ? (outInput.innerText = inputRef.trim()) : `Anonymous`;
 };
